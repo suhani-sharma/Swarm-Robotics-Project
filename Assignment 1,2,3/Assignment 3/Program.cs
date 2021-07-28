a3bd61e530b438282fe4ff41c00d54efdda42ac3
suhani-sharma <82612028+suhani-sharma@users.noreply.github.com>
2021-07-28 15:06:25 +0530

    Update Program.cs

--- Assignment 1,2,3/Assignment 3/Program.cs
+++ Assignment 1,2,3/Assignment 3/Program.cs
@@ -1,1 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playercontroller : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray,out hit))
+            {
+                //move the agent
+                agent.SetDestination(hit.point);
+            }
+
+        }
+    }
+}
 
